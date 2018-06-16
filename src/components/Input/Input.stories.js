7f9ccf106bd8e56cd41e3c5203e42b4fce14750e
paxarpp <pavelpp1974@gmail.com>
2018-06-16 22:09:06 +0300

    animate label complite

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 import Input from './index';
 
 storiesOf('Input', module)
-  .add('Input type text', () => <Input type={'text'} placeholder={'holder'} />)
-  .add('Input type text disabled', () => <Input type={'text'} placeholder={'holder'} disabled />)
-  .add('Input type password', () => <Input type={'password'} placeholder={'place'} />);
+  .add('Input type text', () => <Input type={'text'} placeholder={'text'} />)
+  .add('Input type text disabled', () => <Input type={'text'} placeholder={'disable'} disabled />)
+  .add('Input type password', () => <Input type={'password'} placeholder={'password'} />);
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Input = ({ placeholder, ...props }) => {
-  return (
-    <Wrapper>
-      <InpWrap {...props} />
-      <LabelWrap>{placeholder}</LabelWrap>
-    </Wrapper>
-  );
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+  }
+  inputField = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+  render() {
+    const { placeholder, ...props } = this.props;
+    const { value } = this.state;
+    return (
+      <Wrapper>
+        <InpWrap {...props} onChange={this.inputField} />
+        <LabelWrap value={value}>{placeholder}</LabelWrap>
+      </Wrapper>
+    );
+  }
+}
+Input.propTypes = {
+  placeholder: PropTypes.string
 };
+
 const InpWrap = styled.input`
   background-color: transparent;
   border: none;
@@ -17,15 +37,17 @@ const InpWrap = styled.input`
   outline: none;
   height: 3rem;
   width: 100%;
-  font-size: 16px;
+  font-size: 1rem;
   margin: 0 0 8px 0;
   padding: 0;
   box-shadow: none;
   box-sizing: content-box;
   transition: box-shadow 0.3s, border 0.3s;
   :disabled {
-    color: rgba(0, 0, 0, 0.42);
     border-bottom: 1px dotted rgba(0, 0, 0, 0.42);
+  }
+  :disabled + label {
+    color: rgba(0, 0, 0, 0.42);
   }
   :focus + label {
     top: 0;
@@ -38,7 +60,8 @@ const InpWrap = styled.input`
 `;
 const LabelWrap = styled.label`
   position: absolute;
-  top: 1rem;
+  top: ${props => (props.value ? '0' : '1rem')};
+  font-size: ${props => (props.value ? '0.7rem' : '1rem')};
   left: 0;
   transition: all 0.3s;
 `;
