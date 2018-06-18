3514febe8dd18681d9d45ac65988cf260688a47b
paxarpp <pavelpp1974@gmail.com>
2018-06-18 09:41:56 +0300

    bug style fix

--- src/components/Preloader/index.js
+++ src/components/Preloader/index.js
@@ -5,15 +5,15 @@ import PropTypes from 'prop-types';
 const Preloader = ({ ...props }) => (
   <Wrapper {...props}>
     <SpinnerLayer>
-      <Circle_clipper_left>
+      <Left>
         <CircleL />
-      </Circle_clipper_left>
+      </Left>
       <GapPatch>
         <Circle />
       </GapPatch>
-      <Circle_clipper_right>
+      <Right>
         <CircleR />
-      </Circle_clipper_right>
+      </Right>
     </SpinnerLayer>
   </Wrapper>
 );
@@ -21,52 +21,24 @@ Preloader.propTypes = {
   color: PropTypes.string
 };
 const left_spin = keyframes`
-  from {
-    transform: rotate(130deg);
-  }
-  50% {
-    transform: rotate(-5deg);
-  }
-  to {
-    transform: rotate(130deg);
-  }
+  from { transform: rotate(130deg); }
+  50% { transform: rotate(-5deg); }
+  to { transform: rotate(130deg); }
 `;
 const right_spin = keyframes`
-from {
-  transform: rotate(-130deg);
-}
-50% {
-  transform: rotate(5deg);
-}
-to {
-  transform: rotate(-130deg);
-}
+  from { transform: rotate(-130deg); }
+  50% { transform: rotate(5deg);}
+  to { transform: rotate(-130deg); }
 `;
 const fill_unfill_rotate = keyframes`
-  12.5% {
-    transform: rotate(135deg);
-  }
-  25% {
-    transform: rotate(270deg);
-  }
-  37.5% {
-    transform: rotate(405deg);
-  }
-  50% {
-    transform: rotate(540deg);
-  }
-  62.5% {
-    transform: rotate(675deg);
-  }
-  75% {
-    transform: rotate(810deg);
-  }
-  87.5% {
-    transform: rotate(945deg);
-  }
-  to {
-    transform: rotate(1080deg);
-  }
+  12.5% { transform: rotate(135deg); }
+  25% { transform: rotate(270deg); }
+  37.5% { transform: rotate(405deg); }
+  50% { transform: rotate(540deg); }
+  62.5% { transform: rotate(675deg); }
+  75% { transform: rotate(810deg); }
+  87.5% { transform: rotate(945deg); }
+  to { transform: rotate(1080deg); }
 `;
 const small = `
   width: 36px;
@@ -87,6 +59,7 @@ const Wrapper = styled.div`
   ${props => props.small && small};
   ${props => props.big && big};
   animation: ${rotateWrap} 1333ms linear infinite;
+  box-sizing: border-box;
 `;
 const SpinnerLayer = styled.div`
   position: absolute;
@@ -95,6 +68,7 @@ const SpinnerLayer = styled.div`
   border-color: #26a69a;
   opacity: 1;
   animation: ${fill_unfill_rotate} 5332ms cubic-bezier(0.4, 0, 0.2, 1) infinite both;
+  box-sizing: inherit;
 `;
 const GapPatch = styled.div`
   position: absolute;
@@ -104,28 +78,32 @@ const GapPatch = styled.div`
   height: 100%;
   overflow: hidden;
   border-color: inherit;
+  box-sizing: inherit;
 `;
 const Circle = styled.div`
   width: 1000%;
   left: -450%;
+  border-radius: 50%;
+  box-sizing: inherit;
 `;
-const Circle_clipper_left = styled.div`
+const circle_clipper = `
   display: inline-block;
   position: relative;
   width: 50%;
   height: 100%;
   overflow: hidden;
   border-color: inherit;
+  box-sizing: inherit;
 `;
-const Circle_clipper_right = styled.div`
-  display: inline-block;
-  position: relative;
-  width: 50%;
-  height: 100%;
-  overflow: hidden;
-  border-color: inherit;
+const Left = styled.div`
+  ${circle_clipper};
+  float: left !important;
 `;
-const CircleL = styled.div`
+const Right = styled.div`
+  ${circle_clipper};
+  float: right !important;
+`;
+const styleCircle = `
   width: 200%;
   height: 100%;
   border-width: 3px;
@@ -137,28 +115,20 @@ const CircleL = styled.div`
   top: 0;
   right: 0;
   bottom: 0;
+  box-sizing: inherit;
+`;
+const CircleL = styled.div`
+  ${styleCircle};
   left: 0;
   border-right-color: transparent !important;
   transform: rotate(129deg);
-
   animation: ${left_spin} 1333ms cubic-bezier(0.4, 0, 0.2, 1) infinite both;
 `;
 const CircleR = styled.div`
-  width: 200%;
-  height: 100%;
-  border-width: 3px;
-  border-style: solid;
-  border-color: inherit;
-  border-bottom-color: transparent !important;
-  border-radius: 50%;
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
+  ${styleCircle};
   left: -100%;
   border-left-color: transparent !important;
   transform: rotate(-129deg);
-
   animation: ${right_spin} 1333ms cubic-bezier(0.4, 0, 0.2, 1) infinite both;
 `;
 export default Preloader;
