50838b3976e75cf64c4e1136f158ef0ddf89ee07
paxarpp <pavelpp1974@gmail.com>
2018-06-22 12:44:13 +0300

    style fix

--- src/components/Modal/Modal.stories.js
+++ src/components/Modal/Modal.stories.js
@@ -6,7 +6,6 @@ import Modal from './index';
 const modal = {
   header: 'заголовок модального окна',
   content: 'содержание сообщения, различной длинны и наполненности'
-}
+};
 
-storiesOf('Modal', module)
-  .add('Modal ', () => <Modal handlerClick={action('clicked')} isOpen modal={modal} />);
+storiesOf('Modal', module).add('Modal ', () => <Modal handlerClick={action('clicked')} isOpen modal={modal} />);
