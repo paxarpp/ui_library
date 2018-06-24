43f4425b291adccf19a7cf9ee05e15144217a48b
paxarpp <pavelpp1974@gmail.com>
2018-06-24 14:32:58 +0300

    add preloader4

--- src/components/Preloader/Preloader.stories.js
+++ src/components/Preloader/Preloader.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Preloader1, Preloader2, Preloader3, Preloader7 } from './index';
+import { Preloader1, Preloader2, Preloader3, Preloader4, Preloader7 } from './index';
 
 storiesOf('Preloader', module)
   .add('Preloader1 color=black size=24px', () => <Preloader1 size={'24px'} color={'black'} />)
@@ -10,5 +10,7 @@ storiesOf('Preloader', module)
   .add('Preloader2 color=red size=32px', () => <Preloader2 size={'32px'} color={'red'} />)
   .add('Preloader3  // bgColor если фон отличен от белого ', () => <Preloader3 />)
   .add('Preloader3 color=red size=32px', () => <Preloader3 size={'32px'} color={'red'} />)
+  .add('Preloader4  // bgColor если фон отличен от белого ', () => <Preloader4 />)
+  .add('Preloader4 color=blue size=12px', () => <Preloader4 size={'12px'} color={'blue'} />)
   .add('Preloader7', () => <Preloader7 />)
   .add('Preloader7 size=18px color=red', () => <Preloader7 size={'18px'} color={'red'} />);
--- src/components/Preloader/index.js
+++ src/components/Preloader/index.js
@@ -1,4 +1,5 @@
 export { default as Preloader1 } from './Preloader1';
 export { default as Preloader2 } from './Preloader2';
 export { default as Preloader3 } from './Preloader3';
+export { default as Preloader4 } from './Preloader4';
 export { default as Preloader7 } from './Preloader7';
