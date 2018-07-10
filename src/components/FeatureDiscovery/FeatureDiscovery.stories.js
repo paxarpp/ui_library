68f723377aada86db7e9f9f82aadde4bb92c1a62
paxarpp <pavelpp1974@gmail.com>
2018-07-10 16:57:03 +0300

    fix Futurediscavery

--- src/components/FeatureDiscovery/FeatureDiscovery.stories.js
+++ src/components/FeatureDiscovery/FeatureDiscovery.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import FeatureDiscovery from './index';
 
 storiesOf('FeatureDiscovery', module).add('FeatureDiscovery ', () => (
-  <FeatureDiscovery open header="новинки">
-    всякая особая замануха , пишем сдесь !!!
+  <FeatureDiscovery header="новинки">
+    <span>всякая особая замануха , пишем здесь !!!</span>
   </FeatureDiscovery>
 ));
