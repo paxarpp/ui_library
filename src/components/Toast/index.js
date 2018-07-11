8904ac0608b090173ec0ee09450c32e95d81ddee
paxarpp <pavelpp1974@gmail.com>
2018-07-11 09:49:37 +0300

    animation Toast

--- src/components/Toast/index.js
+++ src/components/Toast/index.js
@@ -1,46 +1,46 @@
 import React, { Component } from 'react';
-import styled, { css, keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 import transition from 'styled-transition-group';
+import styled from 'styled-components';
+import { TransitionGroup } from 'react-transition-group';
 
 class Toast extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      toasts: []
+      items: []
     };
   }
+  onRemove = i => () => {
+    const items = this.state.items.slice();
+    items.splice(i, 1);
+    this.setState({ items });
+  };
+  onAdd = () => {
+    const { items } = this.state;
+    this.setState(
+      {
+        items: items.concat(items[0] ? Math.max(...items) + 1 : 1)
+      },
+      () => setTimeout(this.onRemove(items[items.length]), this.props.duration)
+    );
+  };
 
-  addToast = () => {
-    setTimeout(this.deleteFirstToast, this.props.duration);
-    this.setState({
-      toasts: this.state.toasts.concat('active')
-    });
-  };
-  deleteFirstToast = () => {
-    const { toasts } = this.state;
-    this.setState({
-      toasts: toasts.map((el, indx) => (indx === 0 ? 'end' : 'active'))
-    });
-    setTimeout(this.clearFirstToast, 300);
-  };
-  clearFirstToast = () => {
-    const { toasts } = this.state;
-    this.setState({
-      toasts: toasts.slice(1)
-    });
-  };
   render() {
     const { width, height } = this.props;
-    const { toasts } = this.state;
+    const { items } = this.state;
     return (
       <div>
-        <button onClick={this.addToast}>add</button>
-        {toasts.map((el, indx) => (
-          <Div end={el === 'end' ? true : undefined} index={indx} key={indx} width={width} height={height}>
-            {this.props.children}
-          </Div>
-        ))}
+        <button onClick={this.onAdd}>add</button>
+        <Wrapper>
+          <TransitionGroup>
+            {items.map((id, i) => (
+              <Div key={id} width={width} height={height} timeout={800} onClick={this.onRemove(i)}>
+                {this.props.children}
+              </Div>
+            ))}
+          </TransitionGroup>
+        </Wrapper>
       </div>
     );
   }
@@ -56,50 +56,16 @@ Toast.defaultProps = {
   width: 100,
   height: 50
 };
-const enter = keyframes`
-  0%{
-    transform: translateY(50px);
-    opacity: 0.2;
-  }
-  20%{
-    opacity: 0.8;
-  }
-  100%{
-    transform: translateY(0);
-    opacity: 1;
-  }
-`;
-const leave = keyframes`
-  0%{
-    transform: translateY(0);
-    opacity: 1;
-  }
-  20%{
-    opacity: 0.4;
-  }
-  40%{
-    opacity: 0.8;
-  }
-  60%{
-    opacity: 0.2;
-  }
-  80%{
-    opacity: 0.8;
-  }
-  100%{
-    transform: translateY(-10px);
-    opacity: 0;
-  }
-`;
-const isEnd = props =>
-  props.end &&
-  css`
-    animation: ${leave} 0.3s forwards;
-  `;
-const Div = styled.div`
+const Wrapper = styled.div`
   position: fixed;
-  top: ${props => 50 + props.index * (props.height + 10)}px;
+  top: 100px;
   right: 100px;
+`;
+const Div = transition.div.attrs({
+  unmountOnExit: true,
+  timeout: 800
+})`
+  margin-bottom: 10px;
   width: ${props => props.width}px;
   height: ${props => props.height}px;
   background-color: #000000aa;
@@ -109,7 +75,24 @@ const Div = styled.div`
   justify-content: center;
   align-items: center;
   overflow: hidden;
-  animation: ${enter} 0.8s;
-  ${isEnd};
+  &:enter { 
+    transform: translateY(50px);
+    opacity: 0.2;
+  }
+  &:enter-active {
+    transform: translateY(0);
+    opacity: 1;
+    transition: all 800ms ease-in;
+  }
+  &:exit { 
+    transform: translateY(0);
+    opacity: 1;
+  }
+  &:exit-active {
+    transform: translateY(-50px);
+    opacity: 0;
+    transition: all 600ms ease-in;
+  }
 `;
+
 export default Toast;
